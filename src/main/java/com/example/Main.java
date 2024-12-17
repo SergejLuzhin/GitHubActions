b2400efe75d26d75073872a44364311343323de6
Sergej <luzhins764@gmail.com>
2024-12-17 22:58:14 +0300

    added http

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,5 +1,24 @@
+package com.example;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Just testing GitHub Pipelines!");
+    public static void main(String[] args) throws IOException {
+        // Читаем порт из переменной окружения или используем 8080 по умолчанию
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+
+        // Создаём HTTP сервер
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", exchange -> {
+            String response = "Just testing Github Actions pipelines!";
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+
+        System.out.println("Server is running on port " + port);
+        server.start();
     }
 }
